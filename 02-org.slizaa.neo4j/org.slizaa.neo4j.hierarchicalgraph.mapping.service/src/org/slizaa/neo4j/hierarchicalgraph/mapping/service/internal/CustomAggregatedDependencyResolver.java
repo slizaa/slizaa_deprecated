77ce83bc132711bd26e5cf3ee4b782a9976b31e5
Gerd Wuetherich <gerd@gerd-wuetherich.de>
2016-09-04 22:35:28 +0200

    ~ added (dummy) CustomAggregatedDependencyResolver

---
+++ 02-org.slizaa.neo4j/org.slizaa.neo4j.hierarchicalgraph.mapping.service/src/org/slizaa/neo4j/hierarchicalgraph/mapping/service/internal/CustomAggregatedDependencyResolver.java
@@ -0,0 +1,54 @@
+package org.slizaa.neo4j.hierarchicalgraph.mapping.service.internal;
+
+import java.util.concurrent.Future;
+
+import org.slizaa.hierarchicalgraph.HGDependency;
+import org.slizaa.neo4j.hierarchicalgraph.IAggregatedDependencyResolver;
+
+public class CustomAggregatedDependencyResolver implements IAggregatedDependencyResolver {
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public Future<?> createNewAggregatedDependencyResolver(HGDependency dependencyToResolve) {
+    
+    // Set<Object> fromNodes = new HashSet<>();
+    // Set<Object> toNodes = new HashSet<>();
+    //
+    // //
+    // List<HGDependency> coreDependencies = getDependency().getCoreDependencies();
+    //
+    // //
+    // for (HGDependency hgDependency : coreDependencies) {
+    // for (Iterator<?> iter = EcoreUtil.getAllContents(Collections.singleton(hgDependency.getFrom())); iter
+    // .hasNext();) {
+    // Object containedElement = iter.next();
+    // if (containedElement instanceof HGNode) {
+    // fromNodes.add(((HGNode) containedElement).getIdentifier());
+    // }
+    // }
+    // for (Iterator<?> iter = EcoreUtil.getAllContents(Collections.singleton(hgDependency.getTo())); iter.hasNext();) {
+    // Object containedElement = iter.next();
+    // if (containedElement instanceof HGNode) {
+    // toNodes.add(((HGNode) containedElement).getIdentifier());
+    // }
+    // }
+    // }
+    //
+    // //
+    // Map<String, String> params = new HashMap<>();
+    // params.put("from", fromNodes.toString());
+    // params.put("to", toNodes.toString());
+    //
+    // //
+    // Future<?> future = getJQAssistantRemoteService().executeCypherQuery(TEMP_HELPER.QUERY, params, (queryResult) -> {
+    // // TODO
+    // System.out.println(queryResult);
+    // resolved = true;
+    // });
+
+    
+    return null;
+  }
+}
