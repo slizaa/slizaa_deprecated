4138019ab43ec5403045cefecac38cf172b4c04d
Gerd Wuetherich <gerd@gerd-wuetherich.de>
2016-10-05 21:24:15 +0200

    ~ fixed the build

--- 01-org.slizaa/org.slizaa.ui.common/src/org/slizaa/ui/common/internal/Activator.java
+++ 01-org.slizaa/org.slizaa.ui.common/src/org/slizaa/ui/common/internal/Activator.java
@@ -10,9 +10,6 @@
  ******************************************************************************/
 package org.slizaa.ui.common.internal;
 
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
@@ -31,7 +28,7 @@ public class Activator extends AbstractUIPlugin {
 
   /** - */
   private Color              _sashBackgroundColor;
-  
+
   /** - */
   private Color              _scrollbarThumbColor;
 
@@ -100,78 +97,5 @@ public class Activator extends AbstractUIPlugin {
    */
   public static Activator getDefault() {
     return plugin;
-  }
-
-  /**
-   * Creates a new {@link IStatus} object reporting a Warning
-   * <p>
-   * </p>
-   * 
-   * @param message
-   *          The error message
-   * @param throwable
-   *          The causing throwable if applicable, otherwise null
-   * @return
-   */
-  public static IStatus newWarning(String message, Throwable throwable) {
-    return new Status(IStatus.WARNING, Activator.PLUGIN_ID, message, throwable);
-  }
-
-  /**
-   * Creates a new {@link IStatus} object reporting an Error
-   * <p>
-   * </p>
-   * 
-   * @param message
-   *          The error message
-   * @param throwable
-   *          The causing throwable if applicable, otherwise null
-   * @return
-   */
-  public static IStatus newError(String message, Throwable throwable) {
-    return new Status(IStatus.ERROR, Activator.PLUGIN_ID, message, throwable);
-  }
-
-  /**
-   * <p>
-   * Constructs a new {@link IStatus} for the given {@link CoreException}.
-   * </p>
-   * 
-   * @param coreException
-   *          The coreException
-   * @param message
-   *          An optional message. If null the message from the coreException will be used
-   * @return
-   */
-  public static IStatus newStatus(CoreException coreException, String message) {
-    String actualMessage = (message == null ? coreException.getMessage() : message);
-    return new Status(coreException.getStatus().getSeverity(), Activator.PLUGIN_ID, actualMessage, coreException);
-  }
-
-  /**
-   * Logs the given error
-   * <p>
-   * </p>
-   * 
-   * @param message
-   *          The message
-   * @param throwable
-   *          The causing throwable if applicable
-   */
-  public static void logError(String message, Throwable throwable) {
-    IStatus error = newError(message, throwable);
-    Activator.getDefault().getLog().log(error);
-  }
-
-  /**
-   * <p>
-   * </p>
-   * 
-   * @param message
-   * @param args
-   */
-  public static void logErrorMessage(String message, Object... args) {
-    IStatus error = newError(String.format(message, args), null);
-    Activator.getDefault().getLog().log(error);
   }
 }
