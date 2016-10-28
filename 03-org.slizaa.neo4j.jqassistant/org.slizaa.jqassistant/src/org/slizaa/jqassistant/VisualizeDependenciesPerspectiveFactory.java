d1f4e15f3283ea6ef2cfa6809917ab460aa73353
Gerd Wütherich <gerd@gerd-wuetherich.de>
2016-10-28 13:27:48 +0200

    ~ savepoint

---
+++ 03-org.slizaa.neo4j.jqassistant/org.slizaa.jqassistant/src/org/slizaa/jqassistant/VisualizeDependenciesPerspectiveFactory.java
@@ -0,0 +1,19 @@
+package org.slizaa.jqassistant;
+
+import org.eclipse.ui.IFolderLayout;
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class VisualizeDependenciesPerspectiveFactory implements IPerspectiveFactory {
+
+  @Override
+  public void createInitialLayout(IPageLayout layout) {
+   IFolderLayout folderLayout =  layout.createFolder("left", IPageLayout.LEFT, 0.2f, IPageLayout.ID_OUTLINE);
+    folderLayout.addView(IPageLayout.ID_OUTLINE);
+    layout.createFolder("right", IPageLayout.RIGHT, 0.6f, IPageLayout.ID_OUTLINE);
+    layout.createFolder("bottom", IPageLayout.BOTTOM, 0.8f, IPageLayout.ID_OUTLINE);
+    layout.createFolder("top", IPageLayout.TOP, 0.6f, IPageLayout.ID_OUTLINE);
+    
+    layout.setEditorAreaVisible(false);
+  }
+}
