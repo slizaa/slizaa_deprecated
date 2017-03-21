127ecb5d8d4bf94373ac07f472a3a23e77756622
Gerd Wütherich <gerd@gerd-wuetherich.de>
2017-03-21 17:12:58 +0100

    ~ added test setup

--- 03-org.slizaa.neo4j/org.slizaa.neo4j.hierarchicalgraph.ui.test/src/org/slizaa/neo4j/hierarchicalgraph/ui/MappingsDialogTest.java
+++ 03-org.slizaa.neo4j/org.slizaa.neo4j.hierarchicalgraph.ui.test/src/org/slizaa/neo4j/hierarchicalgraph/ui/MappingsDialogTest.java
@@ -1,14 +1,42 @@
 package org.slizaa.neo4j.hierarchicalgraph.ui;
 
-import org.junit.Test;
-import org.slizaa.testfwk.ui.AbstractXmiBasedSlizaaPartTest;
+import java.io.IOException;
 
-public class MappingsDialogTest extends AbstractXmiBasedSlizaaPartTest {
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.junit.Test;
+import org.slizaa.neo4j.dbadapter.dsl.DbAdapterDslStandaloneSetup;
+import org.slizaa.neo4j.hierarchicalgraph.mapping.dsl.MappingDslStandaloneSetup;
+import org.slizaa.neo4j.hierarchicalgraph.mapping.dsl.mappingDsl.MappingDescriptor;
+import org.slizaa.testfwk.ui.AbstractSlizaaPartTest;
+
+public class MappingsDialogTest extends AbstractSlizaaPartTest {
 
   @Test
-  public void test() {
+  public void test() throws IOException {
+
+    //
+    MappingDslStandaloneSetup.doSetup();
+    DbAdapterDslStandaloneSetup.doSetup();
     
+    ResourceSet resourceSet = new ResourceSetImpl();
+    URI uri = URI.createFileURI("src/org/slizaa/neo4j/hierarchicalgraph/ui/SimpleFlatPackagesMapping.mapping");
+    Resource resource = resourceSet.getResource(uri, true);
+    MappingDescriptor mappingDescriptor = (MappingDescriptor) resource.getContents().get(0);
     
+    //
+    System.out.println(mappingDescriptor);
+//    for (org.xtext.example.myDsl.Class clazz : model.getElements()) {
+//     System.out.println(clazz.getName());  
+//    }
     
+//    XtextParser parser = new XtextParser();
+//    MappingDescriptor mappingDescriptor = (MappingDescriptor) parser
+//        .parse(this.getClass().getResource("SimpleFlatPackagesMapping.mapping").openStream());
+//
+//    //
+//    System.out.println(mappingDescriptor);
   }
 }
